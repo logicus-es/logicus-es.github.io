ce3a4e3a25a459da93c90f4e84267c4531f0cfdc
logicus-es <90924451+logicus-es@users.noreply.github.com>
2021-09-22 10:15:49 +0200

    Update materials.js

--- js/materials.js
+++ js/materials.js
@@ -4,18 +4,16 @@
     Campos:
         - title: El título que aparecerá como encabezado de la página 
         - description: Descripción del contenido del vídeo que aparecerá en la página
-        - poster: Nombre de la imagen que aparecerá como portada del vídeo, (en la carpeta docs/videos/images/)
-        - link: Nombre del archivo del vídeo en la carpeta docs/videos (debe estar en formato .webm)
+        - link: Nombre del archivo del vídeo en Youtube
         - visibility: 0 para no visible, 1 para visible.
-        - complementary : string con el código html con información/materiales complementarios. Si no se precisa dejar como una cadena vacía. Utilizar la carpeta docs/videos/files (o subcarpeta para archivos adicionales).
+        - complementary : string con el código html con información/materiales complementarios. Si no se precisa dejar como una cadena vacía.
 */
 
 var videos =
     [
-        {   "title": "Conoce LogicUS", 
+        {   "title": "Presentación LogicUS", 
             "description":"Presentamos una perspectiva general del proyecto LogicUS, sus objetivos, herramientas, instalación y uso, materiales y participantes en el proyecto.", 
-            "poster":"logicus_intro/logicus_intro.png",
-            "link":"logicus_intro.webm", 
+            "link":"https://youtu.be/MgMMMrYn8kU", 
             "complementary":"", 
             "visibility":1
         },
@@ -31,8 +29,8 @@ var videos =
         - title: El título que aparecerá como encabezado de la página 
         - description: Descripción corta del notebook que aparecerá en la página
         - section: LP, LPO ó OT (OTHER)
-        - link: ruta al archivo a descargar (ex. "notebooks/archivo.md")
-        - sollink : ruta al archivo de la solución (ex. "notebooks/archivo_sol.md")
+        - link: ruta al archivo de google drive
+        - sollink : ruta al archivo de la solución en google drive
         - visibility: 0 para no visible, 1 para visible solo el enunciado, 2 para visible enunciado y solución
 */
 
