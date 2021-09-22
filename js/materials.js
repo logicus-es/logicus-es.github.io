6830d1a0c0689b9c050ed0f834f93d6061187202
logicus-es <90924451+logicus-es@users.noreply.github.com>
2021-09-22 10:22:33 +0200

    Update materials.js

--- js/materials.js
+++ js/materials.js
@@ -4,7 +4,7 @@
     Campos:
         - title: El título que aparecerá como encabezado de la página 
         - description: Descripción del contenido del vídeo que aparecerá en la página
-        - link: Nombre del archivo del vídeo en Youtube (recuerda añadir el embed/ antes del código. Ejemplo https://youtu.be/embed/MgMMMrYn8kU
+        - link: Nombre del archivo del vídeo en Youtube (recuerda añadir el embed/ antes del código. Ejemplo https://www.youtube.com/embed/MgMMMrYn8kU
         - visibility: 0 para no visible, 1 para visible.
         - complementary : string con el código html con información/materiales complementarios. Si no se precisa dejar como una cadena vacía.
 */
